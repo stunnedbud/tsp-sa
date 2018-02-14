7ec9c2820b37a33d75e487d41994c0c41ac42d8d
stunnedbud <jantonio.lr@ciencias.unam.mx>
2018-02-14 16:43:47 -0600

    Se agregaron funciones para calcular costo

--- graph.jl
+++ graph.jl
@@ -16,19 +16,12 @@ type Node
     degree::Int
 end
 
-
 type Graph
     nodes::Array{Node,1}
 end
 
-# Might be better to use JLD to save and load the graph object, rather than remaking it each time
-#function populate_graph(jld_file::String)
-
-#end
-
 function populate_graph(cities_file::AbstractString, connections_file::AbstractString)
     net = Graph(Node[])    
-
     open(cities_file) do f
         lines = readlines(f)
         for l in lines
@@ -39,7 +32,6 @@ function populate_graph(cities_file::AbstractString, connections_file::AbstractS
             push!(net.nodes, node)
         end
     end
-
     open(connections_file) do f
         lines = readlines(f)
         for l in lines
@@ -52,9 +44,8 @@ function populate_graph(cities_file::AbstractString, connections_file::AbstractS
             push!(net.nodes[from_index].connections, Connection(to_index, distance)) 
             net.nodes[from_index].degree += 1
         end
-    end
-    
-    return net
+    end 
+    net
 end
 
 # Might be useful
@@ -70,94 +61,69 @@ function subgraph(graph::Graph, cities::Array{Int,1})
         end
         push!(nodes, Node(c, og.name, og.country, og.population, og.latitude, conns, length(conns)))
     end
-    return Graph(nodes)
+    Graph(nodes)
 end
 
-# Returns true if solution (path) is acceptable, ie. connections exist between all consecutive nodes
-function acceptable(g::Graph, path::Array{Int,1})
-    i = 1
-    println(path)
-    while i < length(path)
-        from = path[i]
-        to = path[i+1]
-        con_exists = false
-        for c in g.nodes[from].connections
-            if c.to_node == to
-                con_exists = true
-                break
-            end
+function shuffle_subgraph(g::Graph, s::Array{Int,1}, seed)
+    srand(seed)
+    shuffle(MersenneTwister(seed), s)
+end
+
+##
+# Cost
+##
+function weight_average(g::Graph, s::Array{Int,1})
+    weights = Float64[]
+    for n in s
+        for c in g.nodes[n].connections
+            push!(weights, c.distance)
         end
-        if !con_exists
-            println("failed at "*string(i))
-            return false
-        end
-        i += 1
     end
-    true
+    sum(weights) / length(weights)
 end
 
-
-function random_acceptable(g::Graph, cities::Array{Int,1}, seed)
-    srand(seed)
-    return shuffle(MersenneTwister(seed), cities)
-end
-
-#=
-function random_acceptable2(g::Graph, cities::Array{Int,1}, seed)	
-    println("Will find hamiltonian path for:")
-    println(cities)
-    println("throwing darts...")
-    srand(seed)
-    attempts = 0
-    while true
-        attempts += 1
-        seed2 = convert(Int, floor(100000000rand())) # because we cannot use a float as seed
-        shuffled = shuffle(MersenneTwister(seed2), cities) # might not be most efficient but this way it is reproducible
-        n = pop!(shuffled)
-        path = [n]
-        tried_both_ends = false
-        while length(shuffled) > 0
-            n_neighbors = [m.to_node for m in g.nodes[n].connections] # lets transform conns to list of ints to make life easier
-            found_path = false
-            position = 0
-            for c in shuffled
-                position += 1
-                if c in n_neighbors
-                    deleteat!(shuffled,position)
-                    push!(path, c)
-                    n = c
-                    found_path = true
-                    break
-                end
-            end
-            if !found_path
-                if !tried_both_ends
-                    n = path[1]
-                    tried_both_ends = true
-                else
-                    break
-                end
+function calc_punishment(g::Graph, punishment_factor::Float64)
+    max = 0.0
+    for n in g.nodes
+        for c in n.connections
+            if c.distance > max
+                max = c.distance
             end
         end
-        if length(path) == length(cities)
+    end
+    max*punishment_factor
+end
+
+function augmented_weight(g::Graph, u::Int, v::Int, punishment::Float64)
+    
+    distance = -1
+    for c in g.nodes[u].connections
+        if c.to_node == v
+            distance = c.distance
             break
         end
-        # else
-        #println(length(path))
-        #println(path)
+    end       
+    if distance != -1
+        return distance
+    else
+        return punishment
     end
-    
-    println("FOUND ACCEPTABLE SOLUTION AFTER "*string(attempts)*" ATTEMPTS:")
-    println(path)
-    
-    return path
 end
-=#
+
+function cost(g::Graph, s::Array{Int, 1}, punishment::Float64)
+    ws = 0.0
+    for i in 2:length(s)
+        ws += augmented_weight(g, i-1, i, punishment)
+    end
+    ws / weight_average(g,s)
+end
+
 
 cities = [22,74,109,113,117,137,178,180,200,216,272,299,345,447,492,493,498,505,521,572,607,627,642,679,710,717,747,774,786,829,830,839,853,857,893,921,935,986,1032,1073]
 seed = 1234567890
+punishment_factor = 5.5
 
 g = populate_graph("cities.txt", "connections.txt")
 
-solution = random_acceptable(g, cities, seed)
-print(solution)
+p = calc_punishment(g, punishment_factor)
+println(cost(g, cities, p))
